9880e9efc2d43a974ee31ac9732722f930cc7881
Connor Bowley <connor.bowley@kitware.com>
2022-01-17 13:47:47 -0500

    ENH: Pipeline convert labelmap volume to scalar volume

--- PipelineModules/PipelineModulesLib/VolumesWrapping.py
+++ PipelineModules/PipelineModulesLib/VolumesWrapping.py
@@ -27,3 +27,29 @@ class ExportScalarVolumeToLabelMapVolume(SinglePiecePipeline):
     labelmap = slicer.mrmlScene.AddNewNodeByClass('vtkMRMLLabelMapVolumeNode')
     slicer.modules.volumes.logic().CreateLabelVolumeFromVolume(slicer.mrmlScene, labelmap, inputNode)
     return labelmap
+
+@slicerPipeline
+class ExportLabelMapVolumeToScalarVolume(SinglePiecePipeline):
+  @staticmethod
+  def GetName():
+    return "Export LabelMap Volume to Scalar Volume"
+  @staticmethod
+  def GetInputType():
+    return 'vtkMRMLLabelMapVolumeNode'
+  @staticmethod
+  def GetOutputType():
+    return 'vtkMRMLScalarVolumeNode'
+  @staticmethod
+  def GetDependencies():
+    return ['Volumes']
+  @staticmethod
+  def GetParameters():
+    return []
+  
+  def __init__(self):
+    super().__init__()
+
+  def _RunImpl(self, inputNode):
+    scalar = slicer.mrmlScene.AddNewNodeByClass('vtkMRMLScalarVolumeNode')
+    slicer.modules.volumes.logic().CreateScalarVolumeFromVolume(slicer.mrmlScene, scalar, inputNode)
+    return scalar
