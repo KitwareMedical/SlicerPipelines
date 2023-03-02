98e28fbead10b4aa3bf6d7c3680056ae62d6b36e
Connor Bowley <connor.bowley@kitware.com>
2023-03-02 13:42:14 -0500

    ENH: Pipelines to export models to segmentations

--- PipelineModules/_PipelineModules/SegmentationsWrapping.py
+++ PipelineModules/_PipelineModules/SegmentationsWrapping.py
@@ -1,9 +1,21 @@
+from typing import Annotated
+
 import vtk
 import slicer
+
+from slicer.parameterNodeWrapper import (
+    parameterPack,
+
+    Decimals,
+    Default,
+    Minimum,
+    SingleStep,
+)
 
 from MRMLCorePython import (
     vtkMRMLLabelMapVolumeNode,
     vtkMRMLModelNode,
+    vtkMRMLScalarVolumeNode,
     vtkMRMLSegmentationNode,
 )
 
@@ -44,3 +56,55 @@ def exportLabelMapToSegmentation(labelMap: vtkMRMLLabelMapVolumeNode) -> vtkMRML
     slicer.modules.segmentations.logic().ImportLabelmapToSegmentationNode(labelMap, seg)
     seg.CreateClosedSurfaceRepresentation()
     return seg
+
+
+@slicerPipeline(name="Export Model to Segmentation - Reference Volume", categories=["Conversions", "Model Operations"])
+def exportModelToSegmentationReferenceVolume(model: vtkMRMLModelNode,
+                                             referenceVolume: vtkMRMLScalarVolumeNode) -> vtkMRMLSegmentationNode:
+    segmentationNode = slicer.mrmlScene.AddNewNodeByClass('vtkMRMLSegmentationNode')
+    segmentationNode.CreateDefaultDisplayNodes()
+    segmentationNode.SetReferenceImageGeometryParameterFromVolumeNode(referenceVolume)
+    slicer.modules.segmentations.logic().ImportModelToSegmentationNode(model, segmentationNode)
+    return segmentationNode
+
+
+@parameterPack
+class SegmentationAndReferenceVolume:
+    segmentation: vtkMRMLSegmentationNode
+    referenceVolume: vtkMRMLScalarVolumeNode
+
+
+@slicerPipeline(name="Export Model to Segmentation - Spacing", categories=["Conversions", "Model Operations"])
+def exportModelToSegmentationSpacing(model: vtkMRMLModelNode,
+                                     spacingX: Annotated[float, Minimum(0), Default(0.1), Decimals(2), SingleStep(0.01)],
+                                     spacingY: Annotated[float, Minimum(0), Default(0.1), Decimals(2), SingleStep(0.01)],
+                                     spacingZ: Annotated[float, Minimum(0), Default(0.1), Decimals(2), SingleStep(0.01)],
+                                     marginX: Annotated[float, Minimum(0), Default(1), Decimals(1), SingleStep(0.1)],
+                                     marginY: Annotated[float, Minimum(0), Default(1), Decimals(1), SingleStep(0.1)],
+                                     marginZ: Annotated[float, Minimum(0), Default(1), Decimals(1), SingleStep(0.1)]) -> SegmentationAndReferenceVolume:
+    volumeMargin = [marginX, marginY, marginZ]
+    volumeSpacing = [spacingX, spacingY, spacingZ]
+    
+    #create reference volume
+    bounds = [0.]*6
+    model.GetBounds(bounds)
+    imageData = vtk.vtkImageData()
+    imageSize = [int((bounds[axis * 2 + 1] - bounds[axis * 2] + volumeMargin[axis] * 2.0) / volumeSpacing[axis]) for axis in range(3)]
+    imageOrigin = [ bounds[axis * 2] - volumeMargin[axis] for axis in range(3) ]
+    imageData.SetDimensions(imageSize)
+    imageData.AllocateScalars(vtk.VTK_UNSIGNED_CHAR, 1)
+    imageData.GetPointData().GetScalars().Fill(0)
+    referenceVolumeNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLScalarVolumeNode")
+    referenceVolumeNode.SetName(f"{model.GetName()}_ReferenceVolume")
+    referenceVolumeNode.SetOrigin(imageOrigin)
+    referenceVolumeNode.SetSpacing(volumeSpacing)
+    referenceVolumeNode.SetAndObserveImageData(imageData)
+    referenceVolumeNode.CreateDefaultDisplayNodes()
+
+    segmentationNode = slicer.mrmlScene.AddNewNodeByClass('vtkMRMLSegmentationNode')
+    segmentationNode.CreateDefaultDisplayNodes()
+    segmentationNode.SetReferenceImageGeometryParameterFromVolumeNode(referenceVolumeNode)
+    slicer.modules.segmentations.logic().ImportModelToSegmentationNode(model, segmentationNode)
+
+    return SegmentationAndReferenceVolume(segmentationNode, referenceVolumeNode)
+
