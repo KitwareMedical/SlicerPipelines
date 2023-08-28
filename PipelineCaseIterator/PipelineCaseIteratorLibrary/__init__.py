6d44c90a32de9bf2b794899f3ce6cc53dbc8bae0
David Allemang <david.allemang@kitware.com>
2023-08-28 12:16:05 -0400

    COMP: Fix PipelineCaseIteratorLibrary python package
    - Add __init__.py to CMakeLists
    - Add __all__ to __init__.py

--- PipelineCaseIterator/PipelineCaseIteratorLibrary/__init__.py
+++ PipelineCaseIterator/PipelineCaseIteratorLibrary/__init__.py
@@ -1,3 +1,11 @@
 from .Asynchrony import Asynchrony
 from .IteratorParameterFile import IteratorParameterFile
 from .Util import ScopedNode, ScopedDefaultStorageNode, human_sorted
+
+__all__ = [
+    "Asynchrony",
+    "IteratorParameterFile",
+    "ScopedNode",
+    "ScopedDefaultStorageNode",
+    "human_sorted",
+]
